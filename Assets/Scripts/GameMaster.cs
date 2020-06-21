6d78472cb896d033357f48fe8e4509c35a9eb44d
alanthewang <22178748+alanthewang@users.noreply.github.com>
2020-06-21 13:24:06 -0400

    the first time is the hardest

---
+++ Assets/Scripts/GameMaster.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameMaster : MonoBehaviour
+{ 
+    public Text ForceIndicatorText;
+    public Slider xslider;
+    public Slider yslider;
+    public Slider zslider;
+    public Button FindGeoButton;
+    public float minTargetDist = 50;
+    private static GameMaster _instance;
+    public static GameMaster Instance
+    {
+        get
+        {
+            return _instance;
+        }
+
+    }
+
+    private void Start()
+    {
+        _instance = this;
+        FindGeo(true);
+    }
+
+    private void FixedUpdate()
+    {
+        ForceIndicatorText.text = string.Format("Applied force {0:0},{1:0},{2:0}", xslider.value, yslider.value, zslider.value);
+        if (Input.anyKey)
+        {
+            FindGeo(false);
+        }
+    }
+
+    public void FindGeo(bool enable)
+    {
+        FindGeoButton.interactable = !enable;
+        Camera.main.GetComponent<LerpCam>().enabled = enable;
+    }
+
+    public void AddForce(Vector3 force)
+    {
+
+    }
+}
